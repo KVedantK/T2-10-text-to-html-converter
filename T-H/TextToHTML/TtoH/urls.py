0ed59ac6a5e6e812371c5c3400742fa2700eca3b
KVedantK <kulvedant2000@gmail.com>
2021-05-01 23:07:53 +0530

    Final Model

---
+++ T-H/TextToHTML/TtoH/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path,include
+from . import views
+
+urlpatterns = [
+    path('',views.Home),
+    path('Convert',views.Convert),
+    path('Download', views.download_file)
+]
